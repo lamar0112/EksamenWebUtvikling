68c97497a17b5b7b0a01b73e78378755840691ef
lamar0112 <lasa020@student.kristiania.no>
2025-12-04 15:38:24 +0100

    ny versjon

--- FootballAPI/Controllers/FinanceController.cs
+++ FootballAPI/Controllers/FinanceController.cs
@@ -34,7 +34,37 @@ public FinanceController(FotballContext dbContext)
          
     }
     // - PUT/POST for å oppdatere finance
-    
+
+    [HttpPost]
+    public async Task<ActionResult> Post (Finance finance)
+    {
+        try
+        {
+            dbContext.Finances.Add(finance);
+        await dbContext.SaveChangesAsync();
+        return Created();
+        }
+        catch
+        {
+            return StatusCode (500);
+        }
+         
+    }
+   
+    [HttpPut]
+    public async Task<IActionResult> Put(Finance finance)
+    {
+        try
+        {
+            dbContext.Entry(finance).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+        catch
+        {
+            return StatusCode (500);
+        }
+    }
 
 
     // - Eget endpoint for lån
