f9d2b68b371efc3d447c0f852133fa50d3336184
Abdullah-1903 <abah005@student.kristiania.no>
2025-12-04 18:40:03 +0100

    nyeste

--- Fotball/fotball/src/App.tsx
+++ Fotball/fotball/src/App.tsx
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-function App() {
-  
-=======
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import AppRouting from "./routing/AppRouting";
 
 function App() {
-  const [count, setCount] = useState(0);
->>>>>>> 45c529069778dcbbed7ca1a965647180e902edca
-
-  return (
-    <>
-      <h1 className="text-4xl text-emerald-400 font-bold">Tailwind 4 funker!</h1>
-
-    </>
-  );
+  return <AppRouting />;
 }
 
 export default App;
